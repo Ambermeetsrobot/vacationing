9d35f694bb3d9a89720bbf19853e47bd69a0de49
Ambermeetsrobot <amber@empiricalux.com>
2016-02-20 12:37:05 -0800

    add display logic for two possible form entries

---
+++ js/newfile.js
@@ -0,0 +1,5 @@
+// var options = ["activity-level", "socialness", "environment", "climate"];
+//
+// options.forEach(function(option) {
+//   var selection = $("select#").val();
+// });
--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,21 @@
 $(document).ready(function() {
-  $("#vacation-options form").submit(function(event) {
-    var age = parseInt($("input#age").val());
-    var options = ["activity-level", "socialness", "environment", "climate"];
-
-    options.forEach(function(option) {
-      var selection = $("select#").val();
-    });
-    // var xx = $("select#xx").val();
-    // var xx = $("select#xx").val();
-
+  $('div#vacation-options').submit(function(event) {
+    var age = parseInt($('input#age').val());
+    var activitylevel = $('select#activity-level').val();
+    var socialness = $('select#socialness').val();
+    var environment = $('select#environment').val();
+    var climate = $('select#climate').val();
 //Need help figuring this part out
-    if (age >=xx && activity-level === 'xx' && socialness="xx" ** environment === "xx" && climate ==="xx" {
-      $(#vacation1).show();
-      $(#vacation-options).hide();
+    if (age < 18) {
+      $('#under-18').show();
+      $('#vacation-options').hide();
     }
+    alert("I got this far!");
 
-    else if (age >=xx && activity-level === 'xx' && socialness="xx" ** environment === "xx" && climate ==="xx" {
-      $(#vacation2).show();
-      $(#vacation-options).hide();
-    }
-
-    else (age >=xx && activity-level === 'xx' && socialness="xx" ** environment === "xx" && climate ==="xx" {
-      $(#vacation3).show();
-      $(#vacation-options).hide();
-    }
-
-
-    //   $("#style").empty().append(profile);
-      $("#vacation1").show();
-      $("#vacation2").show();
-      $("#vacation3").show();
-    // else {
-    //   alert('Please enter your age.');
+    // else if (activitylevel === 'active' && socialness='introvert' && environment === 'natural' && climate === 'cool') {
+    //   $('#vacation1-pct').show();
+    //   $('#vacation-options').hide();
     // }
-    //
-    // event.preventDefault();
+    event.preventDefault();
   });
 });
