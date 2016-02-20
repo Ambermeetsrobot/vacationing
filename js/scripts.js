a497a58350fc5929a2640814c46a44941f2d7f23
Ambermeetsrobot <amber@empiricalux.com>
2016-02-19 21:50:57 -0800

    add html, js, & css framework

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $("#vacation-options form").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var options = ["activity-level", "socialness", "environment", "climate"];
+
+    options.forEach(function(option) {
+      var selection = $("select#").val();
+    });
+    // var xx = $("select#xx").val();
+    // var xx = $("select#xx").val();
+
+//Need help figuring this part out
+    if (age >=xx && activity-level === 'xx' && socialness="xx" ** environment === "xx" && climate ==="xx" {
+      $(#xx).show();
+    }
+
+    else if (age >=xx && activity-level === 'xx' && socialness="xx" ** environment === "xx" && climate ==="xx" {
+      $(#xx).show();
+    }
+
+    else (age >=xx && activity-level === 'xx' && socialness="xx" ** environment === "xx" && climate ==="xx" {
+      $(#xx).show();
+    }
+
+
+    //   $("#style").empty().append(profile);
+      $("#profile1").show();
+      $("#profile2").show();
+      $("#profile3").show();
+    // else {
+    //   alert('Please enter your age.');
+    // }
+    //
+    // event.preventDefault();
+  });
+});
