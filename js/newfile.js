5873321a2d197d7201a484ff3929374f4ec1391f
Ambermeetsrobot <amber@empiricalux.com>
2016-02-20 21:14:42 -0800

    unsuccessful attempt at looping selection variables in scripts.js

--- js/newfile.js
+++ js/newfile.js
@@ -1,5 +1,13 @@
-// var options = ["activity-level", "socialness", "environment", "climate"];
-//
-// options.forEach(function(option) {
-//   var selection = $("select#").val();
-// });
+var options = ["gender", "activity-level", "environment", "climate"];
+
+options.forEach(function(option) {
+  var selection = $("select#").val();
+});
+activitylevel === 'relaxed' || activitylevel === 'active' &&
+
+if (activitylevel === 'active' && environment === 'urban') {
+  $('#vacation-options').hide();
+  $('#vacation-portland').show();
+}
+
+activitylevel === 'relaxed' || activitylevel === 'active'
--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,47 @@
 $(document).ready(function() {
   $('div#vacation-options').submit(function(event) {
     var age = parseInt($('input#age').val());
+    var gender = $('select#gender').val();
     var activitylevel = $('select#activity-level').val();
-    var socialness = $('select#socialness').val();
     var environment = $('select#environment').val();
     var climate = $('select#climate').val();
+    // var options = ["gender", "activity-level", "environment", "climate"];
+    // options.forEach(function(option) {
+    //   var selection = $("select#" + option).val();
+    // )};
+//unsuccessful looping effort; not sure what should go after select
+
+    alert("i got this far!");
 //Need help figuring this part out
     if (age < 18) {
       $('#vacation-options').hide();
       $('#under-18').show();
     }
 
-    if (activitylevel === 'active' && socialness === 'introvert' && environment === 'natural' && climate === 'cool') {
+    if (age > 18 && activitylevel === 'active' && environment === 'natural') {
       $('#vacation-options').hide();
       $('#vacation-pct').show();
     }
-    alert("I got to PCT");
 
-    if (age > 50 && activitylevel === 'relaxed' && climate === 'hot') {
-      $('vacation-options').hide();
-      $('vacation-palmsprings').show();
+    if (age > 18 && activitylevel === 'relaxed' && environment === 'natural' && climate === 'hot') {
+      $('#vacation-options').hide();
+      $('#vacation-palmsprings').show();
     }
 
-    alert("I got to Palm Springs");
-
-    if (environment === 'urban' && climate === 'cool') {
-      $('vacation-options').hide();
-      $('vacation-sanfrancisco').show();
+    if (age > 18 && activitylevel === 'relaxed' && environment === 'natural' && climate === 'cool') {
+      $('#vacation-options').hide();
+      $('#vacation-bigsur').show();
     }
-    alert("I got to SF");
 
-    if (environment === 'urban' && climate === 'hot') {
-      $('vacation-options').hide();
-      $('vacation-losangeles').show();
+    if (age > 18 && climate === 'cool' &&  environment === 'urban') {
+      $('#vacation-options').hide();
+      $('#vacation-portland').show();
     }
-    alert("I got to LA");
 
-    else {
-      $('vacation-options').hide();
-      $('vacation-portland').show();
+    if (age > 18 && climate === 'hot' && environment === 'urban') {
+      $('#vacation-options').hide();
+      $('#vacation-losangeles').show();
     }
-    alert("I got to pdx");
 
     event.preventDefault();
   });
